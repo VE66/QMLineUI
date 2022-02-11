6368a04003fbfea03818b8acfae07add86405d77
VE66 <942914231@qq.com>
2022-02-11 15:33:26 +0800

    0.1.1

--- QMLineUI/Classes/QMChatUI/QMChatVC/ViewController/QMChatRoomViewController.m
+++ QMLineUI/Classes/QMChatUI/QMChatVC/ViewController/QMChatRoomViewController.m
@@ -1772,19 +1772,7 @@
 
 - (void)textViewDidChange:(UITextView *)textView {
     if (textView.text.length > 0) {
-//        if (!self.isRobot) {
-//            isShowAssociatsInput = isShowAssociatsWithAgent;
-//            if ([QMConnect sdkGetIsInputMonitor]) {
-//                
-//                long nowTime = [NSString getTimeStamp:[NSDate date]];
-//                if ((dateTime == 0) || (nowTime - dateTime > 1)) {
-//                    dateTime = nowTime;
-//                    [QMConnect sdkInputMonitor:textView.text successBlock:^{
-//                    } failBlock:^{
-//                    }];
-//                }
-//            }
-//        }
+
         if (isShowAssociatsInput) {
             NSString *robotType = [QMConnect sdkRobotType];
             robotType = [robotType isEqual: @""] ? @"xbot" : robotType;
